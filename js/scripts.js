8c4c6b4cbf1a653834172b8f55bf646246400b47
RS <roboszyn@gmail.com>
2017-02-25 04:19:02 +0000

    exercise 8.4

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+
+var a = prompt('Podaj długość podstawy trójkąta');
+
+var h = prompt('Podaj wysokość trójkąta');
+
+var triangleArea = a*h/2;
+
+alert('Pole trójkąta wynosi ' + triangleArea);
+
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+
+console.log(triangleArea);
